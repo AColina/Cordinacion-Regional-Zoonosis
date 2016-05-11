a9c2c3dfbf3c6b969b181483070e705d994678c8
angel.colina <angel.colina@MGFP-L02.megagroup.local>
2016-05-11 09:29:38 -0430

    restructuacion del proyecto

---
+++ src/ve/zoonosis/model/listeners/TemplateListeners.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 clases.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ve.zoonosis.model.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import ve.zoonosis.controller.modulos.casos.BandejaCasosController;
+import ve.zoonosis.controller.modulos.jornadasvacunaciones.BandejaJornadaVacunacionController;
+import ve.zoonosis.model.components.AbstractInternalListener;
+import ve.zoonosis.vistas.Template;
+
+/**
+ *
+ * @author clases
+ */
+public class TemplateListeners {
+
+    private static Template TEMPLATE;
+
+    public static void inicializarTemplate(Template template) {
+        TemplateListeners.TEMPLATE = template;
+    }
+
+    public static Bandeja getJornadaVacunacionBandeja() {
+        return new Bandeja("Jornadas de Vacunación", BandejaJornadaVacunacionController.class);
+    }
+
+    public static Bandeja getCasosBandeja() {
+        return new Bandeja("Casos", BandejaCasosController.class);
+    }
+
+    public static class Bandeja extends AbstractInternalListener implements ActionListener {
+
+        private final String titulo;
+        private final Class clase;
+
+        public Bandeja(String titulo, Class clase) {
+            super(TEMPLATE);
+            this.titulo = titulo;
+            this.clase = clase;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            crearInternalFrame(titulo, clase);
+
+        }
+
+    }
+
+}
